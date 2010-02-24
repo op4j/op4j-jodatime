2e2ca3830b9dba0674f90fdac7ef5f4024874181
Soraya Sánchez <soraya.schz@gmail.com>
2010-02-24 21:20:56 +0000

    Minor changes due to modifications in op4j
    git-svn-id: https://op4j.svn.sourceforge.net/svnroot/op4j/trunk/op4j-jodatime@494 2d6b427a-3b9d-4b24-b9d1-46c41b8758ae

--- src/main/java/org/op4j/contrib/executables/functions/conversion/ToDateMidnight.java
+++ src/main/java/org/op4j/contrib/executables/functions/conversion/ToDateMidnight.java
@@ -29,16 +29,14 @@ import java.util.Locale;
 import org.apache.commons.lang.LocaleUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.Validate;
-import org.javaruntype.type.Type;
-import org.javaruntype.type.Types;
 import org.joda.time.Chronology;
 import org.joda.time.DateMidnight;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-import org.op4j.exceptions.FunctionExecutionException;
+import org.op4j.exceptions.ExecutionException;
+import org.op4j.functions.AbstractNullAsNullFunction;
 import org.op4j.functions.ExecCtx;
-import org.op4j.functions.converters.AbstractNullAsNullConverter;
 
 /**
  * 
@@ -353,7 +351,7 @@ public final class ToDateMidnight {
 	//
 	
 	
-	private static abstract class BaseToDateTime<T> extends AbstractNullAsNullConverter<DateMidnight, T> {
+	private static abstract class BaseToDateTime<T> extends AbstractNullAsNullFunction<DateMidnight, T> {
 
 		DateTimeZone dateTimeZone = null;
 		Chronology chronology = null;
@@ -376,11 +374,6 @@ public final class ToDateMidnight {
 			Validate.notNull(chronology, "chronology can't be null");
 						
 			this.chronology = chronology;
-		}
-
-		public Type<? extends DateMidnight> getResultType(
-				Type<? extends T> targetType) {
-			return Types.forClass(DateMidnight.class);
 		}
 	}
 	
@@ -551,7 +544,7 @@ public final class ToDateMidnight {
 			DateTimeFormatter dateTimeFormatter = null;
 		    if (this.locale == null) {
 		        if (StringUtils.contains(this.pattern, "MMM") || StringUtils.contains(this.pattern, "EEE")) {
-		        	throw new FunctionExecutionException(
+		        	throw new ExecutionException(
 							"The use of MMM, MMMM, EEE or EEEE as part of the date pattern requires a Locale");
 	            }
 		        dateTimeFormatter = DateTimeFormat.forPattern(this.pattern);        
@@ -734,7 +727,7 @@ public final class ToDateMidnight {
 		@Override
 		public DateMidnight nullAsNullExecute(List<Integer> object, ExecCtx ctx) throws Exception {
 			if (object.size() < 1 || object.size() > 3) {
-				throw new FunctionExecutionException(
+				throw new ExecutionException(
 						"Integer arguments list for DateMidnight conversion should of size " +
 						"between 1 and 3 (year, month, day). Size " + object.size() + " is not valid.");
 			}			
@@ -777,7 +770,7 @@ public final class ToDateMidnight {
 		@Override
 		public DateMidnight nullAsNullExecute(Integer[] object, ExecCtx ctx) throws Exception {
 			if (object.length < 1 || object.length > 3) {
-				throw new FunctionExecutionException(
+				throw new ExecutionException(
 						"Integer arguments array for DateMidnight conversion should of size " +
 						"between 1 and 3 (year, month, day). Size " + object.length + " is not valid.");
 			}		
@@ -823,7 +816,7 @@ public final class ToDateMidnight {
 		@Override
 		public DateMidnight nullAsNullExecute(List<String> object, ExecCtx ctx) throws Exception {
 			if (object.size() < 1 || object.size() > 3) {
-				throw new FunctionExecutionException(
+				throw new ExecutionException(
 						"String arguments list for DateMidnight conversion should of size " +
 						"between 1 and 3 (year, month, day). Size " + object.size() + " is not valid.");
 			}			
@@ -869,7 +862,7 @@ public final class ToDateMidnight {
 		@Override
 		public DateMidnight nullAsNullExecute(String[] object, ExecCtx ctx) throws Exception {
 			if (object.length < 1 || object.length > 3) {
-				throw new FunctionExecutionException(
+				throw new ExecutionException(
 						"String arguments array for DateMidnight conversion should of size " +
 						"between 1 and 3 (year, month, day). Size " + object.length + " is not valid.");
 			}			
--- src/main/java/org/op4j/contrib/executables/functions/conversion/ToDateTime.java
+++ src/main/java/org/op4j/contrib/executables/functions/conversion/ToDateTime.java
@@ -29,16 +29,14 @@ import java.util.Locale;
 import org.apache.commons.lang.LocaleUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.Validate;
-import org.javaruntype.type.Type;
-import org.javaruntype.type.Types;
 import org.joda.time.Chronology;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-import org.op4j.exceptions.FunctionExecutionException;
+import org.op4j.exceptions.ExecutionException;
+import org.op4j.functions.AbstractNullAsNullFunction;
 import org.op4j.functions.ExecCtx;
-import org.op4j.functions.converters.AbstractNullAsNullConverter;
 
 /**
  * 
@@ -355,7 +353,7 @@ public final class ToDateTime {
 	//
 	
 	
-	private static abstract class BaseToDateTime<T> extends AbstractNullAsNullConverter<DateTime, T> {
+	private static abstract class BaseToDateTime<T> extends AbstractNullAsNullFunction<DateTime, T> {
 
 		DateTimeZone dateTimeZone = null;
 		Chronology chronology = null;
@@ -378,11 +376,6 @@ public final class ToDateTime {
 			Validate.notNull(chronology, "chronology can't be null");
 			
 			this.chronology = chronology;
-		}
-
-		public Type<? extends DateTime> getResultType(
-				Type<? extends T> targetType) {
-			return Types.forClass(DateTime.class);
 		}
 	}
 	
@@ -553,7 +546,7 @@ public final class ToDateTime {
 			DateTimeFormatter dateTimeFormatter = null;
 		    if (this.locale == null) {
 		        if (StringUtils.contains(this.pattern, "MMM") || StringUtils.contains(this.pattern, "EEE")) {
-		        	throw new FunctionExecutionException(
+		        	throw new ExecutionException(
 							"The use of MMM, MMMM, EEE or EEEE as part of the date pattern requires a Locale");
 	            }
 		        dateTimeFormatter = DateTimeFormat.forPattern(this.pattern);        
@@ -740,7 +733,7 @@ public final class ToDateTime {
 		@Override
 		public DateTime nullAsNullExecute(List<Integer> object, ExecCtx ctx) throws Exception {
 			if (object.size() < 4 || object.size() > 7) {
-				throw new FunctionExecutionException(
+				throw new ExecutionException(
 						"Integer arguments list for DateTime conversion should of size " +
 						"between 4 and 7 (year, month, day, hour, minute, second, millisecond). Size " + object.size() + " is not valid.");
 			}			
@@ -787,7 +780,7 @@ public final class ToDateTime {
 		@Override
 		public DateTime nullAsNullExecute(Integer[] object, ExecCtx ctx) throws Exception {
 			if (object.length < 4 || object.length > 7) {
-				throw new FunctionExecutionException(
+				throw new ExecutionException(
 						"Integer arguments array for DateTime conversion should of size " +
 						"between 4 and 7 (year, month, day, hour, minute, second, millisecond). Size " + object.length + " is not valid.");
 			}		
@@ -837,7 +830,7 @@ public final class ToDateTime {
 		@Override
 		public DateTime nullAsNullExecute(List<String> object, ExecCtx ctx) throws Exception {
 			if (object.size() < 4 || object.size() > 7) {
-				throw new FunctionExecutionException(
+				throw new ExecutionException(
 						"String arguments list for DateTime conversion should of size " +
 						"between 4 and 7 (year, month, day, hour, minute, second, millisecond). Size " + object.size() + " is not valid.");
 			}			
@@ -889,7 +882,7 @@ public final class ToDateTime {
 		@Override
 		public DateTime nullAsNullExecute(String[] object, ExecCtx ctx) throws Exception {
 			if (object.length < 4 || object.length > 7) {
-				throw new FunctionExecutionException(
+				throw new ExecutionException(
 						"String arguments array for DateTime conversion should of size " +
 						"between 4 and 7 (year, month, day, hour, minute, second, millisecond). Size " + object.length + " is not valid.");
 			}			
